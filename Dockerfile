9b5fe2e5f82ff46d6b49da3838d37b86e1cf9a0f
keefeere <keefeere@ukr.net>
2021-03-24 16:08:50 +0200

    Stage 1 completed, build success

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,16 @@
 FROM maven:latest
 LABEL Description="Multistage dockerfile" Vendor="keefeere" Version="1.0"
 
+
+ARG NEXUS_ADMIN_PASS
+ENV NEXUS_ADMIN_PASS=$NEXUS_ADMIN_PASS
+
 COPY settings.xml /usr/share/maven/ref/
 COPY ./app /app
 
-RUN mvn -B -f /app/pom.xml -s /usr/share/maven/ref/settings-docker.xml clean deploy
+RUN echo Admin pass $NEXUS_ADMIN_PASS
+
+RUN mvn  -f /app/pom.xml -s /usr/share/maven/ref/settings.xml clean deploy -Dmaven.test.skip=true
+#-B -f
+
+FROM openjdk:8-jre-alpine
--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 
- docker stop $(docker ps -a -q)
- docker rm $(docker ps -a -q)
- docker rmi $(docker images -q)
+   docker stop $(docker ps -a -q)
+   docker rm $(docker ps -a -q)
+   docker rmi $(docker images -q)
 
-sh ./insta-nexus-master/go.sh
+  sh /vagrant/insta-nexus-master/go.sh
 
-docker build -t java.builder /vagrant
+export NEXUS_ADMIN_PASS=`docker exec -it nexus cat /nexus-data/admin.password`
+echo $NEXUS_ADMIN_PASS
+
+
+docker build -t java.builder --build-arg NEXUS_ADMIN_PASS=${NEXUS_ADMIN_PASS} /vagrant
