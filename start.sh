709dadec378d340fbe173cb4904d4d9420627bb0
keefeere <keefeere@ukr.net>
2021-03-25 13:51:56 +0200

    some corections after clean run

--- start.sh
+++ start.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 
-  #    docker stop $(docker ps -a -q)
-  #    docker rm $(docker ps -a -q)
-  #    docker rmi -f $(docker images -q)
+      # docker stop $(docker ps -a -q)
+      # docker rm $(docker ps -a -q)
+      # docker rmi -f $(docker images -q)
 
-  #  sh /vagrant/insta-nexus-master/go.sh
+cd /vagrant/insta-nexus-master
+sh ./go.sh
+cd /vagrant
 
-export NEXUS_ADMIN_PASS=`docker exec -it nexus cat /nexus-data/admin.password`
+export NEXUS_ADMIN_PASS=`docker exec -i nexus cat /nexus-data/admin.password`
 
 #Testing
 #echo $NEXUS_ADMIN_PASS
 
 
-sudo userdel devops
-sudo useradd devops --shell=/bin/false -p '*'
+ sudo userdel devops
+ sudo useradd devops --shell=/bin/false -p '*'
 
 
-
-
- docker build --tag javabuildrunner --build-arg NEXUS_ADMIN_PASS=${NEXUS_ADMIN_PASS} /vagrant
- docker kill javabuildrunner 
- docker run -it --rm -d -p 8080:8080 --name javabuildrunner javabuildrunner
-  ( docker logs -f javabuildrunner & ) | grep -q "welcomePageHandlerMapping"
-  curl localhost:8080
+  docker build --tag javabuildrunner --build-arg NEXUS_ADMIN_PASS=${NEXUS_ADMIN_PASS} /vagrant
+  docker kill javabuildrunner 
+  docker run -it --rm -d -p 8080:8080 --name javabuildrunner javabuildrunner
+   ( docker logs -f javabuildrunner & ) | grep -q "welcomePageHandlerMapping"
+   curl localhost:8080
 
